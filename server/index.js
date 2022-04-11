5f9b8850ccdb7e844c42d13c494abeb4e578820b
lkhuu96 <khuu96@gmail.com>
2022-04-11 16:41:04 -0700

    set the get request route to '/api/garage'

--- server/index.js
+++ server/index.js
@@ -1,11 +1,29 @@
 require('dotenv/config');
 const express = require('express');
+const db = require('./db');
 const errorMiddleware = require('./error-middleware');
 const staticMiddleware = require('./static-middleware');
 
 const app = express();
 
 app.use(staticMiddleware);
+
+const jsonMiddleware = express.json();
+
+app.use(jsonMiddleware);
+
+app.get('/api/garage', (req, res) => {
+  const sql = `
+    select *
+      from "vehicles"
+
+  `;
+  db.query(sql)
+    .then(result => {
+      res.json(result.rows);
+    })
+    .catch(err => console.error(err));
+});
 
 app.use(errorMiddleware);
 
