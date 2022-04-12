2eb7c63925489c52284f844a5bfe2e5efd86b5f4
lkhuu96 <khuu96@gmail.com>
2022-04-11 19:14:54 -0700

    Define a loadCars() function that will send a fetch request to get the year, make, and model of all cars in the user owns

--- client/app.jsx
+++ client/app.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import Home from './pages/home';
+// import Home from './pages/home';
 import Navbar from './components/navbar';
+import MyCars from './pages/my-garage';
 
 export default class App extends React.Component {
   render() {
     return (
       <>
         <Navbar />
-        <Home />
+        <MyCars />
       </>
     );
   }
--- client/pages/my-garage.jsx
+++ client/pages/my-garage.jsx
@@ -1,1 +1,22 @@
-// import React from 'react';
+import React from 'react';
+
+class MyCars extends React.Component {
+  constructor(props) {
+    super(props);
+    this.loadCars = this.loadCars.bind(this);
+  }
+
+  loadCars() {
+    fetch('/api/garage')
+      .then(cars => cars.json())
+      .then(result => result);
+  }
+
+  render() {
+    return (
+      <div>{this.loadCars()}</div>
+    );
+  }
+}
+
+export default MyCars;
