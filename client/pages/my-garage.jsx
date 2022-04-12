082ff72a6a9e4b2e033f27f730f58d1209e1fe7e
lkhuu96 <khuu96@gmail.com>
2022-04-12 14:55:39 -0700

    Create an 'add-car' component that renders a modal form for the user to input the car they own.

--- client/pages/my-garage.jsx
+++ client/pages/my-garage.jsx
@@ -1,6 +1,51 @@
-import React from 'react';
-import Card from 'react-bootstrap/card';
+import React, { useState } from 'react';
+import { Card, Modal, Button, Form } from 'react-bootstrap';
 
+function CarForm(form) {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = event => {
+    event.preventDefault();
+    setShow(true);
+  };
+  const handleSubmit = event => {
+    event.preventDefault();
+  };
+
+  return (
+    <>
+      <a className='text-reset' href="#" onClick={handleShow}>
+        <i className="bi fs-1  bi-plus-circle-fill"></i>
+      </a>
+      <Modal show={show} onHide={handleClose} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Add Vehicle</Modal.Title>
+        </Modal.Header>
+        <Form onSubmit={handleSubmit}>
+          <Modal.Body>
+            <Form.Group className='mb-3' controlId='year'>
+              <Form.Control type='text' placeholder='Year'></Form.Control>
+            </Form.Group>
+            <Form.Group className='mb-3' controlId='make'>
+              <Form.Control type='text' placeholder='Make'></Form.Control>
+            </Form.Group>
+            <Form.Group className='mb-3' controlId='model'>
+              <Form.Control type='text' placeholder='Model'></Form.Control>
+            </Form.Group>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button type='submit' variant="primary" onClick={handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Form>
+      </Modal>
+    </>
+  );
+}
 class MyCars extends React.Component {
   constructor(props) {
     super(props);
@@ -11,6 +56,10 @@ class MyCars extends React.Component {
 
   componentDidMount() {
     this.getCars();
+  }
+
+  toggleForm(show) {
+
   }
 
   getCars() {
@@ -46,7 +95,10 @@ class MyCars extends React.Component {
         <ul className="list-group list-group-flush list-unstyled">
           {this.state.cars.length > 0 ? this.state.cars.map(car => this.renderCar(car)) : <h3 className='text-center p-5'>No Cars To Display</h3>}
         </ul>
-        <a href="#" className='text-reset'><i className="bi fs-1  bi-plus-circle-fill"></i></a>
+        <a href='#' className='text-reset'></a>
+        <div>
+          <CarForm />
+        </div>
       </>
     );
   }
