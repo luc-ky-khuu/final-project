5d38ac59001d3b870974afc93ef27e66e33ef627
lkhuu96 <khuu96@gmail.com>
2022-04-12 11:32:59 -0700

    fixed spacing for desktop menu

--- client/components/menu.jsx
+++ client/components/menu.jsx
@@ -5,10 +5,10 @@ class Menu extends React.Component {
   render() {
     return (
       <div className='rounded-3 bg-white border border-secondary shadow d-none d-lg-block flex-grow-1 sticky-top'>
-        <h2>Menu</h2>
+        <h2 className='m-0 list-group-item border-0'>Menu</h2>
         <ul className="list-group list-group-flush">
           <a className='text-decoration-none' href='#garage'>
-            <li className="border-0 bg-transparent list-group-item list-group-item-action">
+            <li className="p-3 fs-5 border-0 bg-transparent list-group-item list-group-item-action">
               Garage
             </li>
           </a>
