2a43dbd00c08b76dd5317ba643e908c87fa7a694
lkhuu96 <khuu96@gmail.com>
2022-05-03 15:42:36 -0700

    udpated confirmation message to match the content being deleted

--- client/pages/all-records.jsx
+++ client/pages/all-records.jsx
@@ -248,7 +248,7 @@ class AllRecords extends React.Component {
         <Modal size='sm' centered show={this.state.deleteModal} onHide={this.toggleDeleteModal}>
           <Modal.Body>
             <p className='fs-4 m-0'>
-              Are you sure you want to remove this car?  This will delete all of your data.
+              Are you sure you want to remove this record?
             </p>
           </Modal.Body>
           <Modal.Footer className='work-sans'>
