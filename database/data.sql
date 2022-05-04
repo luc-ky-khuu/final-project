6593289edebe9e08c1ff1497868b4f560cceceb6
lkhuu96 <khuu96@gmail.com>
2022-05-04 16:30:23 -0700

    updated mock database to insert receitpUrl

--- database/data.sql
+++ database/data.sql
@@ -17,7 +17,7 @@ insert into "public"."vehicles" (
   2011,
   'Toyota',
   'Prius',
-  null
+  'https://vehicle-expenses-tracker.s3.us-west-1.amazonaws.com/1651706851795.jpg'
 ),
 (
   1,
@@ -32,111 +32,127 @@ insert into "public"."records" (
   "maintenanceName",
   "datePerformed",
   "mileage",
-  "cost"
+  "cost",
+  "receiptUrl"
 ) values (
   1,
   'Oil change',
   '2022-02-26',
   199821,
-  60
+  60,
+  null
 ),
 (
   1,
   'Tire rotation',
   '2022-02-26',
   199821,
-  10
+  10,
+  null
 ),
 (
   1,
   'Gas',
   '2022-01-26',
   196833,
-  60
+  60,
+  null
 ),
 (
   1,
   'New Engine',
   '2022-01-26',
   196833,
-  3000
+  3000,
+  null
 ),
 (
   1,
   'Hybrid Battery',
   '2022-01-26',
   196833,
-  800
+  800,
+  null
 ),
 (
   1,
   'Blinker Fluid',
   '2021-12-20',
   194821,
-  30
+  30,
+  null
 ),
 (
   1,
   'Tires',
   '2021-11-26',
   191821,
-  400
+  400,
+  null
 ),
 (
   1,
   'Rims',
   '2021-11-26',
   191821,
-  600
+  600,
+  null
 ),
 (
   1,
   'Oil change',
   '2021-9-26',
   186821,
-  70
+  70,
+  null
 ),
 (
   1,
   'Oil change',
   '2021-08-26',
   180821,
-  60
+  60,
+  null
 ),
 (
   1,
   'Oil change',
   '2021-07-26',
   17221,
-  60
+  60,
+  null
 ),
 (
   1,
   'Car freshener',
   '2022-03-15',
   201922,
-  5
+  5,
+  null
 ),
 (
   2,
   'Oil',
   '2021-12-30',
   126882,
-  60
+  60,
+  null
 ),
 (
   2,
   'Tires',
   '2022-01-26',
   131922,
-  400
+  400,
+  null
 ),
 (
   2,
   'Car freshener',
   '2022-03-01',
   140122,
-  5
+  5,
+  null
 ),
 (
   1,
