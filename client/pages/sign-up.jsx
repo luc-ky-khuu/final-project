19bce4dd4cd3b573f6bedc318e32d2ac0dc791d2
lkhuu96 <khuu96@gmail.com>
2022-05-06 13:57:22 -0700

    created a sign-up jsx page

---
+++ client/pages/sign-up.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class SignUp extends React.Component {
+
+  render() {
+    return (
+      <>
+        <div>
+          test
+        </div>
+      </>
+    );
+  }
+}
+
+export default SignUp;
