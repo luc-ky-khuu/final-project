1b728798e87a9a1ede874182200e15b8156d581f
lkhuu96 <khuu96@gmail.com>
2022-05-06 14:18:06 -0700

    form takes in user input and updates state

--- client/pages/sign-up.jsx
+++ client/pages/sign-up.jsx
@@ -2,6 +2,21 @@ import React from 'react';
 import { Form, Button } from 'react-bootstrap';
 
 class SignUp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+
+  }
 
   render() {
     return (
@@ -13,11 +28,11 @@ class SignUp extends React.Component {
           <Form className='w-50 p-5 bg-white'>
             <Form.Group className="mb-3 text-start" controlId="newUsername">
               <Form.Label>Username</Form.Label>
-              <Form.Control type="text" name='username' placeholder="Enter username" />
+              <Form.Control onChange={this.onChange} type="text" name='username' placeholder="Enter username" />
             </Form.Group>
             <Form.Group className="mb-3 text-start" controlId="newPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" name='username' placeholder="Password" />
+              <Form.Control onChange={this.onChange} type="password" name='password' placeholder="Password" />
             </Form.Group>
             <Button className='w-100' variant="primary" type="submit">
               Sign Up
