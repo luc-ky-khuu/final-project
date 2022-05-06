ea5bffae8c8b26ac6c80cfccb0b7208662873d46
lkhuu96 <khuu96@gmail.com>
2022-05-06 13:59:30 -0700

    added menu item for signup page that takes user to sign up page

--- client/components/menu.jsx
+++ client/components/menu.jsx
@@ -8,11 +8,16 @@ class Menu extends React.Component {
         <div id='menu' className='pe-5 bg-white'>
           <h2 className='m-0 list-group-item border-0'>Menu</h2>
           <ul className='list-group list-group-flush'>
-            <a className='text-decoration-none' href='#garage'>
-              <li className='p-3 fs-5 border-0 bg-transparent list-group-item list-group-item-action'>
+            <li className='p-3 fs-5 border-0 bg-transparent list-group-item list-group-item-action'>
+              <a className='text-decoration-none text-reset' href='#garage'>
                 Garage
-              </li>
-            </a>
+              </a>
+            </li>
+            <li className='p-3 fs-5 border-0 bg-transparent list-group-item list-group-item-action'>
+              <a className='text-decoration-none text-reset' href='#'>
+                SignUp
+              </a>
+            </li>
           </ul>
         </div>
       </div>
