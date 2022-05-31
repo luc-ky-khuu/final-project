a0e52a2a806c66b57c637c86b73a82a388aa0268
lkhuu96 <khuu96@gmail.com>
2022-05-31 13:56:36 -0700

    edited menu so when user is not logged in, only sign in button is present and when user logs in, garage link is visible

--- client/components/menu.jsx
+++ client/components/menu.jsx
@@ -1,6 +1,25 @@
 import React from 'react';
-
+import Context from '../lib/vehicleContext-context';
 class Menu extends React.Component {
+  isLoggedIn() {
+    return (
+      <li className='p-3 fs-5 border-0 bg-transparent list-group-item list-group-item-action'>
+        <a className='text-decoration-none text-reset' href='#garage'>
+          Garage
+        </a>
+      </li>
+    );
+  }
+
+  isNotLoggedIn() {
+    return (
+      <li className='p-3 fs-5 border-0 bg-transparent list-group-item list-group-item-action'>
+        <a className='text-decoration-none text-reset' href='#sign-in'>
+          Sign-In
+        </a>
+      </li>
+    );
+  }
 
   render() {
     return (
@@ -8,21 +27,12 @@ class Menu extends React.Component {
         <div id='menu' className='pe-5 bg-white'>
           <h2 className='m-0 list-group-item border-0'>Menu</h2>
           <ul className='list-group list-group-flush'>
-            <li className='p-3 fs-5 border-0 bg-transparent list-group-item list-group-item-action'>
-              <a className='text-decoration-none text-reset' href='#garage'>
-                Garage
-              </a>
-            </li>
-            <li className='p-3 fs-5 border-0 bg-transparent list-group-item list-group-item-action'>
-              <a className='text-decoration-none text-reset' href='#sign-in'>
-                Sign-In
-              </a>
-            </li>
+            {this.context.user ? this.isLoggedIn() : this.isNotLoggedIn()}
           </ul>
         </div>
       </div>
     );
   }
 }
-
+Menu.contextType = Context;
 export default Menu;
